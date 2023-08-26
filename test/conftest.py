332e8695d3e5503221f2ea78718fe7fe526c33d4
jieun1128 <5656jieun@naver.com>
2023-08-26 19:26:44 +0900

    add test

--- test/conftest.py
+++ test/conftest.py
@@ -1,7 +1,31 @@
 import pytest
-from preprocess.main import Preprocess
+from unittest.mock import patch
+from pyspark.sql import SparkSession, dataframe
+from spark.silver.event_processors import AuthDataFrameProcessor
+from spark.gold.listen_data_join import ListenDataJoinProcessor
+from collections.abc import Generator
+from typing import Any
 
 
 @pytest.fixture(scope="module")
-def preprocess():
-    return Preprocess(conn_id="TEST")
+def test_processor():
+    return AuthDataFrameProcessor("test_bucket")
+
+
+@pytest.fixture(scope="module")
+def test_gold_processor():
+    return ListenDataJoinProcessor(
+        "test_bucket", "test_host", 6789, "test_database", "test_user", "test_password"
+    )
+
+
+@pytest.fixture(scope="module")
+def mock_spark_session():
+    spark = SparkSession.builder.master("local[*]").appName("test_app").getOrCreate()
+    return spark
+
+
+@pytest.fixture
+def mock_dataframe():
+    with patch("pyspark.sql.DataFrame") as mock_df:
+        yield mock_df
---
+++ test/test_spark/test_spark_silver.py
@@ -0,0 +1,94 @@
+from unittest.mock import Mock
+from pyspark.sql import SparkSession, dataframe
+from spark.silver.schema import schema
+from spark.silver.event_processors import AuthDataFrameProcessor
+
+
+class TestSilverDataFrameProcessor:
+    def test_get_spark_session(self, test_processor: AuthDataFrameProcessor) -> None:
+        spark = test_processor.get_spark_session("test_app", "local[*]")
+        assert spark is not None
+        assert spark.conf.get("spark.app.name") == "test_app"
+        assert spark.conf.get("spark.master") == "local[*]"
+
+    def test_read_json_file(self, test_processor: AuthDataFrameProcessor) -> None:
+        bucket = "test_bucket"
+        mock_spark = Mock(spec=SparkSession)
+        mock_spark.read.json = Mock()
+        test_date = "2023-08-25"
+        test_id = "auth"
+
+        test_processor.read_json_file(mock_spark, test_date, test_id)
+
+        mock_spark.read.json.assert_called_once_with(
+            f"s3a://{bucket}/{test_id}/{test_date}/{test_id}_event.json",
+            schema=schema[f"{test_id}_events"],
+        )
+
+    def test_save_dataframe_as_parquet(
+        self,
+        test_processor: AuthDataFrameProcessor,
+        mock_dataframe: dataframe.DataFrame,
+    ) -> None:
+        mock_data = Mock()
+        mock_data.rdd.isEmpty.return_value = False
+        mock_dataframe.return_value = mock_data
+
+        test_processor.save_dataframe_as_parquet("2023-08-25", "test_id", mock_data)
+
+        mock_data.write.parquet.assert_called_once_with(
+            "s3a://test_bucket/test_id/2023-08-25/test_id_event", mode="append"
+        )
+
+    def test_add_state_name(
+        self, test_processor: AuthDataFrameProcessor, mock_spark_session: SparkSession
+    ) -> None:
+
+        test_data = mock_spark_session.createDataFrame(
+            [("CA", "Los Angeles"), ("NY", "New York"), ("TX", "Houston")],
+            ["state", "city"],
+        )
+
+        result_df = test_processor.add_state_name(mock_spark_session, test_data)
+
+        expected_data = mock_spark_session.createDataFrame(
+            [
+                ("CA", "Los Angeles", "California"),
+                ("NY", "New York", "New York"),
+                ("TX", "Houston", "Texas"),
+            ],
+            ["state", "city", "stateName"],
+        )
+
+        assert result_df.collect() == expected_data.collect()
+
+    def test_drop_table(
+        self, test_processor: AuthDataFrameProcessor, mock_spark_session: SparkSession
+    ) -> None:
+        test_data = mock_spark_session.createDataFrame(
+            [
+                (1, "A", "B", "12345", "John", "Doe", 12.34, 56.78, "user1"),
+                (2, "C", "D", "67890", "Jane", "Smith", 45.67, 89.12, "user2"),
+            ],
+            [
+                "itemInSession",
+                "sessionId",
+                "zip",
+                "firstName",
+                "lastName",
+                "lon",
+                "lat",
+                "userId",
+            ],
+        )
+
+        result_df = test_processor.drop_table(test_data)
+
+        assert "itemInSession" not in result_df.columns
+        assert "sessionId" not in result_df.columns
+        assert "zip" not in result_df.columns
+        assert "firstName" not in result_df.columns
+        assert "lastName" not in result_df.columns
+        assert "lon" not in result_df.columns
+        assert "lat" not in result_df.columns
+        assert "userId" not in result_df.columns
